4e441fc6d1233396c1369ceed697bafdf2c9edca
Saachi Kaup <saachi.kaup@gmail.com>
2026-04-30 23:40:50 +0530

    Added model imports

--- Chess.lean
+++ Chess.lean
@@ -1,3 +1,7 @@
 -- This module serves as the root of the `Chess` library.
 -- Import modules here that should be built as part of the library.
 import Chess.Basic
+import Chess.Model
+import Chess.Display
+import Chess.Legal
+import Chess.LegalProofs
