c413bc3c07d2fb039c4ea65ad08ce4c239f2986a
Malhar A. Patel <malharalpeshpatel@gmail.com>
2026-05-01 23:52:49 +0000

    Add `ToString` instances and improve `Repr`

--- Chess/Display.lean
+++ Chess/Display.lean
@@ -23,16 +23,43 @@ def Square.toIcon (square: Square) : String :=
 instance : Repr Square where
   reprPrec square _ := square.toIcon
 
-#check Vector.foldl
-
 def vecToString {n : Nat} (v : Vector Square n) : String :=
   v.foldl (fun s sq => s ++ sq.toIcon) ""
+
+instance : ToString Colour where
+  toString col :=
+    match col with
+    | .white => "white"
+    | .black => "black"
 
 instance : Repr Board where
   reprPrec board _ :=
     board.foldl (fun s row => s ++ vecToString row ++ "\n") ""
 
+instance : ToString Board where
+  toString board :=
+    board.foldl (fun s row => s ++ vecToString row ++ "\n") ""
+
 instance : Repr GameState where
-  reprPrec gs n := reprPrec gs.board n
+  reprPrec gs _ :=
+    let valid := s!"Valid: {gs.valid}\n"
+    let turn := s!"Turn: {gs.turn}\n"
+    let moveNum := s!"Move Number: {gs.moveNum}\n"
+    let board := s!"{gs.board}"
+    valid ++ turn ++ moveNum ++ board
 
+instance : ToString GameState where
+  toString gs :=
+    let valid := s!"Valid: {gs.valid}\n"
+    let turn := s!"Turn: {gs.turn}\n"
+    let moveNum := s!"Move Number: {gs.moveNum}\n"
+    let board := s!"{gs.board}"
+    valid ++ turn ++ moveNum ++ board
 
+instance : Repr <| List GameState where
+  reprPrec lgs n :=
+    lgs.foldl (fun s gs => s ++ "\n" ++ reprPrec gs n) ""
+
+instance : ToString <| List GameState where
+  toString lgs :=
+    lgs.foldl (fun s gs => s ++ "\n" ++ toString gs) ""
