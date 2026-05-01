6b32a3ff05e01ffa014f67188292d5166e2210e4
Malhar A. Patel <malharalpeshpatel@gmail.com>
2026-05-01 23:58:04 +0000

    Import FEN and MoveParser

--- Chess.lean
+++ Chess.lean
@@ -1,7 +1,8 @@
 -- This module serves as the root of the `Chess` library.
 -- Import modules here that should be built as part of the library.
-import Chess.Basic
 import Chess.Model
 import Chess.Display
 import Chess.Legal
 import Chess.LegalProofs
+import Chess.FEN
+import Chess.MoveParser
