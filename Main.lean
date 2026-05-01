b1d1e2f3729f4e2c2ecb5f40791dbea97088102c
Malhar A. Patel <malharalpeshpatel@gmail.com>
2026-05-01 23:58:30 +0000

    Implement crude main function

--- Main.lean
+++ Main.lean
@@ -1,4 +1,26 @@
 import Chess
 
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+def main (args : List String) : IO Unit := do
+  match args with
+  | [] => return ()
+  | fen :: _ =>
+    let state := fenToGameState fen
+    let res ← IO.Process.run { cmd := "python", args := #["script.py", fen] }
+    IO.println s!"Output from LLM: {res}"
+    IO.println "================================="
+    match parsePythonOutputToMoves res with
+    | .error s =>
+      IO.println "Error parsing Json output from LLM to List Move!"
+      throw <| IO.userError s!"{s}"
+    | .ok moves =>
+      if state.isValidMoves moves then
+        let final_state := state.playMoves moves
+        if final_state.isAnyCheckmate then
+          IO.println "Puzzle Successfully Solved!"
+          let states_seq := state.getStatesSequence moves
+          IO.println "The board positions to checkmate:\n"
+          IO.println s!"{states_seq}"
+        else
+          IO.println "Moves do not end in Checkmate!"
+      else
+        IO.println "Moves are invalid!"
